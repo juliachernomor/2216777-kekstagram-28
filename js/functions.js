b401d0dcf6bd245a53b56de771879355bdc6060e
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-02-24 16:43:46 +0300

    fix module2-task1 homework

--- js/functions.js
+++ js/functions.js
@@ -4,14 +4,14 @@ checklength ('проверяемая строка', 18);
 
 //second task 1variant
 const isPalindrome = (string) => {
-  const stringModify = string.replaceAll(' ','').replaceAll('.','').replaceAll(',','').toLowerCase();
+  const stringModify = string.toLowerCase().replace(/[\s.,%]/g,'');
   return stringModify === stringModify.split('').reverse().join('');
 };
 isPalindrome ('Evil fit some kill like me, kill like most, if live.');
 
 //second task 2variant
 const isPalindrome1 = (string) => {
-  const stringModify = string.replaceAll(' ','').replaceAll('.','').replaceAll(',','').toLowerCase();
+  const stringModify = string.toLowerCase().replace(/[^а-яa-z1-9]/gi,'');
   let stringNew = '';
   for (let i = stringModify.length - 1; i >= 0; i--) {
     stringNew += stringModify.at(i);
@@ -20,14 +20,14 @@ const isPalindrome1 = (string) => {
 };
 isPalindrome1('Evil fit some kill like me, kill like most, if live.');
 
-//third task
+//third task1
 const getNumber = (text) => {
   if (typeof text === 'number') {
     return text;
   }
   let textNew = '';
   for (let i = 0; i < text.length; i++) {
-    if (text[i] <= '9' && text[i] >= '0') {
+    if (text[i] <= '9' && text[i] >= '0') {//text[i]  будет равно числовому символу или 0
       textNew = textNew + text[i];
     }
   }
@@ -35,16 +35,20 @@ const getNumber = (text) => {
 };
 getNumber('агент 007');
 
+//third task2
+const getNumber1 = (text) => parseInt(text.replace(/[^\d]/g, ''), 10);
+getNumber1('кефир, батона001');
+
 //fourth task
-function addString (str, count, addRow) {
+const addString = (str, count, addRow) =>{
+  let strModify = str;
   const difference = count - str.length;
   const notEnoughLength = count - str.length - addRow.length;
   if (difference > 0) {
-    str = addRow.slice(0, difference) + str;
+    strModify = addRow.slice(0, difference) + str;
     for (let i = 0; i < notEnoughLength; i++) {
-      str = addRow[0] + str;
+      strModify = addRow[0] + strModify;
     }
-  }
-  return str;
-}
+  }return strModify;
+};
 addString('q', 4, 'we');

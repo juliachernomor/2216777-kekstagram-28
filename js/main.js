5a753172b990ee3e3a8f01e1b307c5f4f6bc1f4e
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-03-01 17:04:11 +0300

    fix: unique: module4-task1

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const DESCRIPTION = [
   'О чем тебе говорит этот снимок?',
   'Изумительная четкость.'
 ];
+
 const NAMES = [
   'Андрей',
   'Алексей',
@@ -16,6 +17,7 @@ const NAMES = [
   'Инга',
   'Жанна',
 ];
+
 const MESSAGE = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -27,25 +29,23 @@ const MESSAGE = [
   'Наталкивает на размышления, да?'
 ];
 
-//генератор случайных целых положительных чисел в заданном диапазоне, возвращает случайное число
+// генератор случайных целых положительных чисел в заданном диапазоне, возвращает случайное число
 const randomNumberGenerator = (max,min) => {
   const lower = Math.ceil(Math.min(Math.abs(max),Math.abs(min)));
   const upper = Math.floor(Math.max(Math.abs(max),Math.abs(min)));
   const result = (Math.random() * (upper - lower + 1)) + lower;
-
   return Math.floor(result);
 };
 
-//функция -возвращает случайный элемент массива(параметр elements - элементы массива)
-
+// функция -возвращает случайный элемент массива(параметр elements - элементы массива)
 const getRandomArrayElement = (elements) => elements[randomNumberGenerator(0, elements.length - 1)];
 
-//проверка на уникальность
-function createUniqueNumber (min,max) {
+// проверка на уникальность
+const createUniqueNumber = (min,max) => {
   const previousValues = [];
-  return () => {
+  return() => {
     let currentValue = randomNumberGenerator(min,max);
-    if(previousValues.length >= (max - min + 1)) {
+    if (previousValues.length >= (max - min + 1)) {
       return null;
     }
     while (previousValues.includes(currentValue)) {
@@ -54,20 +54,32 @@ function createUniqueNumber (min,max) {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
-//обращение к внутренней функции проверки на уникальность
-const uniqueCommentId = createUniqueNumber (1,1000);
+};
 
-const uniquePhotoId = createUniqueNumber (1,25);
-const uniquePhotoUrl = createUniqueNumber (1,25);
+// обращение к внутренней функции проверки на уникальность
+const uniqueCommentId = createUniqueNumber(1,1000);
+const uniquePhotoId = createUniqueNumber(1,25);
+const uniquePhotoUrl = createUniqueNumber(1,25);
+
 
 // объект 2
-const createComments = () => ({
+const createComments = (uniqueCommentAvatar, uniqueCommentMessage) => ({
   id: uniqueCommentId(),
-  avatar: `img/avatar-${randomNumberGenerator(1,6)}.svg`,
-  message: MESSAGE[randomNumberGenerator(1,6)],
+  avatar: `img/avatar-${uniqueCommentAvatar()}.svg`,
+  message: MESSAGE[uniqueCommentMessage()],
   name: getRandomArrayElement(NAMES),
 });
+
+const creareRandomNumber = () => {
+  const uniqueCommentMessage = createUniqueNumber (0,7);
+  const uniqueCommentAvatar = createUniqueNumber (1,6);
+  const countNumber = randomNumberGenerator(1,6);
+  const comments = [];
+  for (let i = 0; i < countNumber; i++) {
+    comments[i] = createComments(uniqueCommentAvatar, uniqueCommentMessage);
+  }
+  return comments;
+};
 
 // объект 1
 const publishPhoto = () => ({
@@ -75,7 +87,7 @@ const publishPhoto = () => ({
   url: `photos/${uniquePhotoUrl()}.jpg`,
   description: getRandomArrayElement(DESCRIPTION),
   likes: randomNumberGenerator(15,200),
-  comments: Array.from({length:randomNumberGenerator(1,6)}, createComments),
+  comments: creareRandomNumber(),
 });
 
 // массив из объектов 1

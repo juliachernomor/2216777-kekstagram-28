f97fa9ee12489bd1d5c68ef07d03f1aaf9c257eb
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2023-03-20 03:34:19 +0300

    comments counter and load

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,4 +1,3 @@
-
 import {isEscapeKey} from './universal.js';
 const bigPicture = document.querySelector('.big-picture');
 const commentList = bigPicture.querySelector('.social__comments');
@@ -15,19 +14,40 @@ const renderComments = (comments) => {
   });
 };
 
+const commentsCount = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
 
-const renderPictureComments = ({url, description, likes, comments}) => {
+
+const showComments = (comments) => {
+  const count = 5;
+  if(comments.length <= count) {
+    commentsCount.textContent = `${commentList.children.length} из ${commentList.children.length} комментариев`;
+    commentsLoader.classList.add('hidden');
+  }
+};
+//   } else if (comments.length > count) {
+//     commentListItem.classList.add('hidden');
+//     commentsLoader.classList.remove('hidden');
+//     commentsLoader.addEventListener('click', (evt) => {
+//       evt.preventDefault();
+//       for (let i = 0; i < num; i++) {
+//         commentListItem[i].classList.remove('hidden');
+//         commentsCount.textContent = `${commentList.children.length - commentList.querySelectorAll('.hidden').length} из ${commentList.children.length} комментариев`;
+//       }
+//     });
+//   }
+// };
+
+
+const renderPictureComments = ({url, description, likes}) => {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = url;
   bigPicture.querySelector('.social__caption').textContent = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
-  bigPicture.querySelector('.comments-count').textContent = comments.length;
+  // bigPicture.querySelector('.comments-count').textContent = comments.length;
 };
 
 const userModalCloseElement = bigPicture.querySelector('.cancel');
 
-const body = document.querySelector('body');
-const commentsCounter = document.querySelector('.social__comment-count');
-const loadNewComments = document.querySelector('.comments-loader');
 
 const onDocumentEscapeKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -36,6 +56,8 @@ const onDocumentEscapeKeydown = (evt) => {
   }
 };
 
+const body = document.querySelector('body');
+
 function closeUserModal () {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
@@ -43,14 +65,16 @@ function closeUserModal () {
 
 userModalCloseElement.addEventListener('click', () => closeUserModal ());
 
+
 const showBigPicture = (data) => {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
-  commentsCounter.classList.add('hidden');
-  loadNewComments.classList.add('hidden');
+  commentsCount.classList.remove('hidden');
+  commentsLoader.classList.remove('hidden');
   document.addEventListener('keydown', onDocumentEscapeKeydown);
   renderPictureComments(data);
   renderComments(data.comments);
+  showComments(data.comments);
 };
 
 export {showBigPicture};
